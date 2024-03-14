0ebaafca635452dd99aaa370f739266f50ce0fb6
klu0926 <lukuoyu@gmail.com>
2024-03-14 18:14:47 +0800

    style: fix tag-link style

--- seeders/20240313015751-tag.js
+++ seeders/20240313015751-tag.js
@@ -7,6 +7,15 @@ const tagSeed = [
   { name: '熊熊上文具' },
   { name: 'Prettier Art' },
   { name: '無日隨記' },
+  { name: 'Mara in mars' },
+  { name: '最貴的男人' },
+  { name: '台灣價值Win' },
+  { name: '愛手藝' },
+  { name: '紙膠帶' },
+  { name: '明信片' },
+  { name: '印章' },
+  { name: '海報' },
+  { name: '本本' },
 ]
 
 /** @type {import('sequelize-cli').Migration} */

e3d9396d0e2857ae0b45d667d63726ef5a789f28
klu0926 <lukuoyu@gmail.com>
2024-04-08 09:42:38 +0800

    debug

--- app.js
+++ app.js
@@ -1,69 +1,76 @@
+try {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('use dotenv')
+    require('dotenv').config()
+  }
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('use dotenv')
-  require('dotenv').config()
-}
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const routes = require('./routes')
-const methodOverride = require('method-override')
-const handlebarHelper = require('./helpers/handlebarHelper')
-const path = require('path')
-const session = require('express-session')
-const checkSessionAuth = require('./middleware/sessionAuth')
-const cors = require('cors')
+  const express = require('express')
+  const app = express()
+  const port = process.env.PORT || 3000
+  const routes = require('./routes')
+  const methodOverride = require('method-override')
+  const handlebarHelper = require('./helpers/handlebarHelper')
+  const path = require('path')
+  const session = require('express-session')
+  const checkSessionAuth = require('./middleware/sessionAuth')
+  const cors = require('cors')
 
 
-// view engine
-const { engine } = require('express-handlebars')
-app.engine(
-  'hbs',
-  engine({
-    defaultLayout: 'main', extname: '.hbs', helpers: handlebarHelper,
+  // view engine
+  const { engine } = require('express-handlebars')
+  app.engine(
+    'hbs',
+    engine({
+      defaultLayout: 'main', extname: '.hbs', helpers: handlebarHelper,
+    })
+  )
+  app.set('view engine', 'hbs')
+  app.set('views', path.join(__dirname, 'views'))
+
+  // cors
+  const corsOption = {
+    origin: [
+      'http://localhost:3001',
+      'https://klu0926.github.io',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+  app.use(cors(corsOption))
+
+  // session
+  const MemoryStore = session.MemoryStore
+  const memoryStore = new MemoryStore()
+  app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  }))
+  app.use(checkSessionAuth)
+  app.use((req, res, next) => {
+    res.locals.user = req.user
+    res.locals.isAuth = req.isAuthenticated
+    next()
   })
-)
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
 
-// cors
-const corsOption = {
-  origin: [
-    'http://localhost:3001',
-    'https://klu0926.github.io',
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  // middleware
+  app.use(express.static('public'))
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(methodOverride('_method'))
+  app.use(routes)
+
+  app.listen(port, () => {
+    console.log('-----------------------------')
+    console.log(`Sever running on port ${port}`)
+    console.log('current NODE_ENV : ' + process.env.NODE_ENV)
+    console.log('-----------------------------')
+  })
+
+
+} catch (err) {
+  console.error('An error occurred during startup: ', err)
 }
-app.use(cors(corsOption))
 
-// session
-const MemoryStore = session.MemoryStore
-const memoryStore = new MemoryStore()
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  store: memoryStore
-}))
-app.use(checkSessionAuth)
-app.use((req, res, next) => {
-  res.locals.user = req.user
-  res.locals.isAuth = req.isAuthenticated
-  next()
-})
 
-// middleware
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(methodOverride('_method'))
-app.use(routes)
-
-app.listen(port, () => {
-  console.log('-----------------------------')
-  console.log(`Sever running on port ${port}`)
-  console.log('current NODE_ENV : ' + process.env.NODE_ENV)
-  console.log('-----------------------------')
-})
